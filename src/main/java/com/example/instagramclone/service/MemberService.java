da585adda42a2b01d876137e03c91dc034362a3a
soongu <naong526@naver.com>
2025-02-12 17:02:39 +0900

    jpa migration with jdbctemplate

--- src/main/java/com/example/instagramclone/service/MemberService.java
+++ src/main/java/com/example/instagramclone/service/MemberService.java
@@ -130,7 +130,8 @@ public class MemberService {
         return Map.of(
                 "message", "로그인에 성공했습니다.",
                 "username", foundMember.getUsername(),
-                "accessToken", jwtTokenProvider.createAccessToken(foundMember.getUsername())
+                "accessToken", jwtTokenProvider.createAccessToken(foundMember.getUsername()),
+                "profileImage", foundMember.getProfileImageUrl()
         );
     }
 }
